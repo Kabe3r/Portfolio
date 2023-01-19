c8812b6a997f000ea8f00d8ca95afcf16a5ad8ca
Ahmed Ali <kaber596@gmail.com>
2023-01-19 22:49:34 +0500

    add logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,8 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <AppProvider>
-    <>
     <Sidebar />
   <Component {...pageProps} />
-    </>
    </AppProvider>
   )
 }
